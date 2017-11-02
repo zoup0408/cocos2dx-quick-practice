bcd146912f4cdad9085ab02377060c3e04703720
zoup0512 <30991006+zoup0512@users.noreply.github.com>
2017-11-02 17:46:30 +0800

    add quick-3.7 samples for learn new api

--- fruit/src/app/scenes/MainScene.lua
+++ fruit/src/app/scenes/MainScene.lua
@@ -1,35 +1,64 @@
-
-local MenuScene = class("MenuScene", function()
-    return display.newScene("MenuScene")
+local MainScene = class("MainScene", function()
+    return display.newScene("MainScene")
 end)
 
-function MenuScene:ctor()
-	-- 1.加载精灵帧
-    display.addSpriteFrames("fruit.plist", "fruit.png")
-
-    -- 2.背景图片
-	display.newSprite("mainBG.png")
-		:pos(display.cx,display.cy)
-		:addTo(self)
-
-	local startBtnImages = {
-		normal = "#startBtn_N.png",
-		pressed = "#startBtn_S.png",
+function MainScene:ctor()
+	local tests = {
+		"Test_NodeFrameEvent",
+		"Test_NodeEvent",
+		"Test_KeypadEvent",
+		"Test_NodeTouchEvent",
+		"Test_AccelerometerEvent",
+		"Test_CocosStudio",
+		"Test_Audio",
 	}
-    
-	-- 3.开始按钮
-  --   cc.ui.UIPushButton.new(startBtnImages, {scale9 = false})
-		-- :onButtonClicked(function(event)
-		-- 	print("TODO: switch to PlayScene!")
-		-- end)
-		-- :align(display.CENTER, display.cx, display.cy - 80)
-		-- :addTo(self)
+
+	local scrollView = ccui.ScrollView:create()
+	scrollView:addTo(self)
+	scrollView:align(display.TOP_CENTER, display.cx, display.top)
+	self.scrollView = scrollView
+
+	local total = 0
+	local btnSize = nil
+	for i = #tests, 1, -1 do
+		local btn = ccui.Button:create()
+		btn:setTitleText(tests[i])
+		btn:setTitleFontSize(24)
+		btn:addTouchEventListener(function(sender, eventType)
+			if 2 == eventType then
+				scrollView:setVisible(false)
+				app:createView(tests[i]):addTo(self)
+			end
+		end)
+		if not btnSize then
+			btnSize = btn:getContentSize()
+		end
+		btn:pos((display.width - btnSize.width) / 2 + btnSize.width / 2,
+				btnSize.height * total + btnSize.height / 2)
+		total = total + 1
+
+		scrollView:addChild(btn)
+	end
+
+	local totalHeight = btnSize.height * total
+	scrollView:setInnerContainerSize(cc.size(display.width, totalHeight))
+	local scrollHeight = display.height
+	if totalHeight < scrollHeight then
+		scrollHeight = totalHeight
+	end
+	scrollView:setContentSize(cc.size(display.width, scrollHeight))
 end
 
-function MenuScene:onEnter()
+function MainScene:openScrollView()
+	self.scrollView:setVisible(true)
 end
 
-function MenuScene:onExit()
+function MainScene:onEnter()
+	print("MainScene onEnter")
 end
 
-return MenuScene
+function MainScene:onExit()
+	print("MainScene onExit")
+end
+
+return MainScene
